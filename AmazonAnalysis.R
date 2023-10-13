8c4f8c9ae0df26473f0dca2bc701c78ef21eb677
ldornon <lucydornon@gmail.com>
2023-10-13 11:16:36 -0600

    Used penalized logistic regression to predict

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -40,4 +40,73 @@ log_preds <-tibble(id =amazon_test$id,
                    ACTION = amazon_predictions$.pred_1)
 vroom_write(x=log_preds, file="./AmazonPreds.csv", delim=",")
 
+#############
+#Penalized Logistic Regression
+#############
+
+My_Recipe <- recipe(ACTION~., data = amazon_train) %>% 
+  step_mutate_at(all_numeric_predictors(),fn= factor) %>% 
+  step_other(all_nominal_predictors(), threshold = .001) %>% 
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION))
+
+my_mod <- logistic_reg(mixture =tune(), penalty = tune()) %>% 
+  set_engine("glmnet")
+
+amazon_workflow <- workflow() %>% 
+  add_recipe(My_Recipe) %>% 
+  add_model(my_mod)
+
+tuning_grid <- grid_regular(penalty(),
+                            mixture(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon_train, v =10 , repeats = 1)
+
+CV_results <- amazon_workflow %>% 
+  tune_grid(resamples = folds,
+            grid = tuning_grid, 
+            metrics = metric_set(roc_auc))
+
+bestTune <- CV_results %>% 
+  select_best("roc_auc")
+
+final_wf <- amazon_workflow %>% 
+  finalize_workflow(bestTune) %>% 
+  fit(data = amazon_train)
+
+Penalized_preds <-final_wf %>% 
+  predict(new_data = amazon_test, type = "prob")
+
+
+penalized_log_preds <-tibble(id =amazon_test$id,
+                             ACTION = Penalized_preds$.pred_1)
+
+vroom_write(x=penalized_log_preds, file="./AmazonPenalizedPreds.csv", delim=",")
+
+head(final_wf)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
