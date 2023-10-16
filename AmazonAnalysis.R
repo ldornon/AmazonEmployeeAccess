ca8548b3243e8a14eb459a22c4dbc0447c7e9522
ldornon <lucydornon@gmail.com>
2023-10-16 15:50:18 -0600

    Used Random Forest for Predictions

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -14,7 +14,7 @@ amazon_test <- vroom("./test.csv")
 
 A_Recipe <- recipe(ACTION~., data = amazon_train) %>% 
   step_mutate_at(all_numeric_predictors(),fn= factor) %>% 
-  step_other(all_nominal_predictors(), threshold = .01) %>% # combines categorical values that occur <5% into an "other" variable
+  step_other(all_nominal_predictors(), threshold = .01) %>% 
   step_dummy(all_nominal_predictors())
 
 prep <- prep(A_Recipe)
@@ -83,18 +83,57 @@ penalized_log_preds <-tibble(id =amazon_test$id,
 
 vroom_write(x=penalized_log_preds, file="./AmazonPenalizedPreds.csv", delim=",")
 
-head(final_wf)
 
 
+##############
+# Random Forest
+#############
 
 
+my_mod <- rand_forest(mtry = tune(),
+                      min_n = tune(),
+                      trees = 500) %>% 
+  set_engine("ranger") %>% 
+  set_mode("classification")
 
 
+My_Recipe <- recipe(ACTION~., data = amazon_train) %>% 
+  step_mutate_at(all_numeric_predictors(),fn= factor) %>% 
+  step_other(all_nominal_predictors(), threshold = .001) %>% 
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION))
 
 
+RF_amazon_workflow <- workflow() %>% 
+  add_recipe(My_Recipe) %>% 
+  add_model(my_mod)
+
+tuning_grid <- grid_regular(mtry(range = c(1,9)),
+                            min_n(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon_train, v= 5, repeats = 1)
 
 
+CV_results <- RF_amazon_workflow %>% 
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(roc_auc))
 
+bestTune <- CV_results %>% 
+  select_best("roc_auc")
+
+final_wf <-
+  RF_amazon_workflow %>% 
+  finalize_workflow(bestTune) %>% 
+  fit(data = amazon_train)
+
+Random_forest_preds <-final_wf %>% 
+  predict(new_data = amazon_test, type = "prob")
+
+Random_preds <-tibble(id =amazon_test$id,
+                             ACTION = Random_forest_preds$.pred_1)
+
+vroom_write(x=Random_preds, file="./AmazonRFPreds.csv", delim=",")
 
 
 
