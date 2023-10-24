8b5ff472707c1438c3437bdee8a36eb56dc5d5c1
ldornon <lucydornon@gmail.com>
2023-10-24 11:16:49 -0600

    Used PCA for Naive Bayes and KNN

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -87,11 +87,9 @@ penalized_log_preds <-tibble(id =amazon_test$id,
 vroom_write(x=penalized_log_preds, file="./AmazonPenalizedPreds.csv", delim=",")
 
 
-
 ##############
 # Random Forest
 #############
-
 
 my_mod <- rand_forest(mtry = tune(),
                       min_n = tune(),
@@ -254,12 +252,84 @@ KNearest_preds <- tibble(id = amazon_test$id,
                        ACTION = KNN_preds$.pred_1)
 vroom_write(x = KNearest_preds, file ="./AmazonKNNPreds.csv", delim = ",")
 
+###################
+# Naive Bayes - PCA
+##################
 
 
+My_Recipe <- recipe(ACTION~., data = amazon_train) %>% 
+  step_mutate_at(all_numeric_predictors(),fn= factor) %>% 
+  step_other(all_nominal_predictors(), threshold = .001) %>% 
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>% 
+  step_normalize(all_predictors()) %>% 
+  step_pca(all_predictors(), threshold = .8)
+prep <- prep(My_Recipe)
+baked <- bake(prep, new_data = amazon_train)
+
+nb_model <- naive_Bayes(Laplace = tune(), smoothness = tune()) %>% 
+  set_mode("classification") %>% 
+  set_engine("naivebayes")
+
+nb_wf <- workflow() %>% 
+  add_recipe(My_Recipe) %>% 
+  add_model(nb_model)
+
+tuning_grid <- grid_regular(Laplace(),
+                            smoothness())
+
+folds <- vfold_cv(amazon_train, v= 5, repeats = 1)
 
 
+CV_results <- nb_wf %>% 
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(roc_auc))
+
+bestTune <- CV_results %>% 
+  select_best("roc_auc")
 
 
+final_wf <- nb_wf %>% finalize_workflow(bestTune) %>% 
+  fit(data = amazon_train)
+
+NB_PCApreds <- final_wf %>% 
+  predict(nb_wf, new_data = amazon_test, type = "prob")
+
+Naive_preds <- tibble(id =amazon_test$id,
+                      ACTION = NB_PCApreds$.pred_1)
+vroom_write(x=Naive_preds, file="./AmazonNBPCAPreds.csv", delim=",")
 
 
+###################
+# KNN - PCA
+##################
+
+knn_model <- nearest_neighbor(neighbors = tune()) %>% 
+  set_mode("classification") %>% 
+  set_engine("kknn")
+
+knn_wf <- workflow() %>% 
+  add_recipe(My_Recipe) %>% 
+  add_model(knn_model)
+
+tuning_grid <- grid_regular(neighbors())
+folds <- vfold_cv(amazon_train, v = 5, repeats = 1)
+
+CV_results <- knn_wf %>% 
+  tune_grid(resamples = folds, 
+            grid = tuning_grid, 
+            metrics = metric_set(roc_auc))
+
+bestTune <- CV_results %>% 
+  select_best("roc_auc")
+
+final_wf <- knn_wf %>% finalize_workflow(bestTune) %>% 
+  fit(data = amazon_train)
+
+KNN_PCApreds <- final_wf %>% 
+  predict(new_data = amazon_test, type = "prob")
+
+KNearest_preds <- tibble(id = amazon_test$id, 
+                         ACTION = KNN_PCApreds$.pred_1)
+vroom_write(x = KNearest_preds, file ="./AmazonKNNPCAPreds.csv", delim = ",")
 
